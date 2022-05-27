7ff940c8ec543d8aba40107eb517542ad8d755b3
AkashRoy5680 <royakash1272@gmail.com>
2022-05-27 04:40:44 +0200

    my order api created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require("dotenv").config();
 const port=process.env.PORT||5000;
 const app=express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors())
@@ -17,7 +17,56 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        
+    const serviceCollection=client.db("parts_hub").collection("services");
+    const reviewCollection=client.db("parts_hub").collection("reviews");
+    const orderCollection=client.db("parts_hub").collection("orders");
+
+    //Load All Services
+    app.get("/service",async(req,res)=>{
+        const query={};
+        const cursor=serviceCollection.find(query);
+        const services=await cursor.toArray();
+        res.send(services);
+      });
+
+    //Load Single Services
+    app.get("/service/:id",async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)};
+        const service=await serviceCollection.findOne(query);
+        res.send(service);
+    });
+    //Filtering Email Orders
+    app.get("/order",async(req,res)=>{
+        const email=req.query.userEmail;
+        const query={email:email};
+        const orders=await orderCollection.find(query).toArray();
+        res.send(orders);
+    })
+
+    
+    app.get("/review",async(req,res)=>{
+        const query={};
+        const cursor=reviewCollection.find(query);
+        const reviews=await cursor.toArray();
+        res.send(reviews);
+    }); 
+      
+    //POST method to add new review and send it to server
+
+    app.post("/review", async (req, res) => {
+        const newItem =req.body;
+        const result = await reviewCollection.insertOne(newItem);
+        res.send(result);
+    });
+
+    //POST method to add order and send it to server
+    app.post("/order",async(req,res)=>{
+        const newOrder=req.body;
+        const result=await orderCollection.insertOne(newOrder);
+        res.send(result);
+    })
+
     }
     
     finally{
